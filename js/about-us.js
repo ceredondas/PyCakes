2d2b2eecd4fe404c68110b5ef201e9dcc7ea604b
CarlosDavoli <davoli_2007@outlook.com>
2022-10-21 19:38:10 -0300

    about us con frases desde api
    21/10/2022 19:38

--- js/about-us.js
+++ js/about-us.js
@@ -20,3 +20,46 @@ slider.querySelector(".controls .down").addEventListener("click",function(){
     slider.querySelector(".wrapper .left div").style.marginTop = `${currentSlide*-100}vh`;
     slider.querySelector(".wrapper .right div").style.marginTop = `${(totalSlides - currentSlide)*-100}vh`;
 });
+
+//GENERADOR DE FRASES POR API
+
+const url = "https://type.fit/api/quotes";
+//da una promise que hay que convertir con .then
+fetch(url).then(response => response.json()).then(data => {
+    // data.forEach(textos => {
+    //     console.log(textos)
+    // })
+    let numberPhrase = randomRange(0, data.length)
+    document.querySelector("#apiPhrase").innerHTML = data[numberPhrase].text;
+    document.querySelector("#author").innerHTML = data[numberPhrase].author;
+
+    numberPhrase = randomRange(0, data.length)
+    document.querySelector("#apiPhrase1").innerHTML = data[numberPhrase].text;
+    document.querySelector("#author1").innerHTML = data[numberPhrase].author;
+
+    numberPhrase = randomRange(0, data.length)
+    document.querySelector("#apiPhrase2").innerHTML = data[numberPhrase].text;
+    document.querySelector("#author2").innerHTML = data[numberPhrase].author;
+
+    numberPhrase = randomRange(0, data.length)
+    document.querySelector("#apiPhrase3").innerHTML = data[numberPhrase].text;
+    document.querySelector("#author3").innerHTML = data[numberPhrase].author;
+
+    numberPhrase = randomRange(0, data.length)
+    document.querySelector("#apiPhrase4").innerHTML = data[numberPhrase].text;
+    document.querySelector("#author4").innerHTML = data[numberPhrase].author;
+
+
+
+    // console.log(frase)
+    // console.log(data[randomRange(0, data.length)].text) // VALE OROOOO ES LA POSTA!!!!!!
+    // document.querySelector("#apiPhrase").innerHTML = frase; 
+})
+.catch(err => console.log(err))
+
+
+
+
+function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  }
